779f90bdfd238ae2e903a29148cbd1116662c9dd
Jachym Putta <jachym.putta@yale.edu>
2024-05-20 19:13:52 -0400

    WIP: importing model

--- app/build.rs
+++ app/build.rs
@@ -1,11 +1,13 @@
 use burn_import::onnx::{ModelGen, RecordType};
 
+const INPUT_ONNX_FILE: &str = "src/model/mnist.onnx";
+const OUT_DIR: &str = "model/";
+
 fn main() {
-    // If the embedded-model, then model is bundled into the binary.
-    // ModelGen::new()
-    //     .input("/shared/typst-detexify/train/my_model_smol_1.onnx")
-    //     .out_dir("model/")
-    //     .record_type(RecordType::Bincode)
-    //     .embed_states(true)
-    //     .run_from_script();
+    ModelGen::new()
+        .input(INPUT_ONNX_FILE)
+        .out_dir(OUT_DIR)
+        .record_type(RecordType::Bincode)
+        .embed_states(true)
+        .run_from_script();
 }
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,28 +1,53 @@
-#![allow(non_snake_case)]
+pub mod model;
+use burn::backend::{wgpu::WgpuDevice, Wgpu};
+pub use model::mnist::Model;
 
 use dioxus::prelude::*;
-use tracing::Level;
+use tracing::{event, Level};
 
 fn main() {
     // Init logger
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    dioxus_logger::init(Level::DEBUG).expect("failed to init logger");
+    type Backend = Wgpu;
+
+    let device = WgpuDevice::default();
+    let model = Model::default();
+    tracing::debug!("debug");
     launch(App);
 }
 
 #[component]
 fn App() -> Element {
     // Build cool things ✌️
+    let mut input_file: Signal<Vec<String>> = use_signal(Vec::new);
 
     rsx! {
         link { rel: "stylesheet", href: "main.css" }
-        img { src: "header.svg", id: "header" }
-        div { id: "links",
-            a { target: "_blank", href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
-            a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { target: "_blank", href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-            a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+        // img { src: "header.svg", id: "header" }
+        div {
+            id: "links",
+            p { "Upload your file" }
+        }
+        input {
+            // tell the input to pick a file
+            r#type: "file",
+            // list the accepted extensions
+            accept: ".png",
+            // pick multiple files
+            multiple: false,
+            onchange: move |evt| {
+                async move {
+                        if let Some(file_engine) = evt.files() {
+                        let files = file_engine.files();
+                        for file_name in &files {
+                            if let Some(file) = file_engine.read_file_to_string(file_name).await
+                            {
+                                input_file.write().push(file);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
