f6e6d9e98ad8ebec73d0665df756b94db784a306
Rashid J. Almheiri <r.muhairi@pm.me>
2024-06-12 23:21:13 +0400

    feat(flake): begin move to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,93 +1,43 @@
 {
-  description = "Typst Detexify";
+  description = "detypstify: Using OCR to convert images of formulas into Typst code.";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
+      imports = [
+        {perSystem = {lib, ...}: {_module.args.l = lib // builtins;};}
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = {
+        l,
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          packages = l.attrValues {
+            inherit (pkgs) just;
+          };
+        };
+
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            ruff.enable = true;
+            alejandra.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      };
+    };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     fenix = {
-      url = "github:nix-community/fenix";
+      url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # nixified-ai = {
-    #   url = "github:DieracDelta/flake/jr";
-    # };
-    # nixified-ai-new = {
-    #   url = "github:nixified-ai/flake";
-    # };
   };
-
-  outputs = inputs@{ self, nixpkgs, utils, fenix}:
-    utils.lib.eachDefaultSystem (system:
-    let
-        fenixStable = with fenix.packages.${system}; combine [
-            (stable.withComponents [ "cargo" "clippy" "rust-src" "rustc" "rustfmt" "llvm-tools-preview" ])
-            targets.wasm32-unknown-unknown.stable.rust-std
-          ];
-        # overlaid = final: prev:
-        #   {
-        #   };
-        # pkgs = import nixpkgs {
-        #   inherit system;
-        #   config = {
-        #     cudaSupport = true;
-        #     allowUnfree = true;
-        #   #   rocmSupport = true;
-        #   };
-        #   overlays = [
-        #     nixified-ai-new.overlays.python-torchCuda
-        #   ];
-        # };
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-          };
-        };
-        in {
-          defaultPackage = self.devShell.${system};
-          devShell = pkgs.mkShell.override { } {
-            shellHook = ''
-              export CARGO_TARGET_DIR="$(git rev-parse --show-toplevel)/app/target_dirs/nix_rustc";
-              # export RUSTFLAGS="-C embed-bitcode=yes -C codegen-units=1 -C opt-level=3 --cfg web_sys_unstable_apis"
-            '';
-            # LD_LIBRARY_PATH = "${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXi}/lib:${pkgs.libxkbcommon}/lib:${pkgs.vulkan-loader}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libGL}/lib"; /* :${pkgs.glib.out}/lib"; */
-            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
-            buildInputs =
-              with pkgs; [
-                cargo-udeps
-                python3
-                # dioxus-cli
-                ninja
-                pkg-config
-                libtorch-bin
-                # (python311.withPackages (ps: with ps; [ (torch.override { cudaSupport =  true; } /* .override { cudaSupport = true; } */ ) /* torchvision */ numpy pip python scikit-learn datasets transformers jiwer jupyter ipywidgets onnxruntime onnx ] ))
-                typst
-                pandoc
-                wasm-bindgen-cli
-                fenixStable
-                # fenix.packages.${system}.rust-analyzer
-                just
-                cargo-expand
-                wasm-pack
-                corepack
-
-                ninja
-                # vulkan-loader
-                # xorg.libX11.dev
-                # xorg.libX11
-                # libxkbcommon
-                nodejs
-                fontconfig
-                # typst
-                # typst-lsp
-                # openblas
-                # cargo-expand
-                # boost
-                # pkg-config
-                openssl.dev
-                openssl
-
-              ];
-          };
-    });
 }
