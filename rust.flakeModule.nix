e5b442dff506e87ce228457e6a70068861531d6e
Rashid J. Almheiri <r.muhairi@pm.me>
2024-06-13 23:01:17 +0400

    feat(flake): Exclude the app from the workspace
    Given the app has a wasm32-unknown-unknown target unfortunately doesn't work well with cargo in a workspace, and breaks a lot of tools.

--- rust.flakeModule.nix
+++ rust.flakeModule.nix
@@ -10,44 +10,66 @@
 
     toolchain = with fenix;
       combine [
-        (stable.withComponents ["cargo" "clippy" "rust-src" "rustc" "rustfmt" "llvm-tools-preview"])
+        (stable.withComponents ["cargo" "clippy" "rust-src" "rustc" "llvm-tools-preview"])
         targets.wasm32-unknown-unknown.stable.rust-std
       ];
 
     craneLib = (inputs.crane.mkLib pkgs).overrideToolchain toolchain;
 
-    commonArgs = {
-      src = ./.;
+    # HACK: Workspace package versions don't seem to be detected by crane
+    readPackageVersion = file:
+      (l.fromTOML (l.readFile file)).package.version;
 
-      version = "0.1.0";
-      strictDeps = true;
-
-      buildInputs = l.optionals pkgs.stdenv.isDarwin [
-        pkgs.libiconv
-        pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-      ];
-
-      doCheck = false;
-    };
+    common.buildInputs = l.optionals pkgs.stdenv.isDarwin (l.attrValues {
+      inherit (pkgs) libiconv;
+      inherit (pkgs.darwin.apple_sdk.frameworks) SystemConfiguration;
+    });
   in {
     treefmt.config.programs.rustfmt.enable = true;
 
-    packages.app = craneLib.buildPackage (commonArgs
-      // {
+    # NOTE: App is not part of the workspace
+    packages.app = let
+      app-config = l.fromTOML (l.readFile ./app/.cargo/config.toml);
+    in
+      craneLib.buildPackage {
         pname = "app";
-        cargoExtraArgs = "--package app --target wasm32-unknown-unknown";
-        CARGO_BUILD_RUSTFLAGS = "-Ctarget-feature=+atomics,+bulk-memory,+mutable-globals -C embed-bitcode=yes -C codegen-units=1 -C opt-level=3 --cfg web_sys_unstable_apis";
-      });
+        src = ./app;
 
-    packages.scraper = craneLib.buildPackage (commonArgs
-      // {
-        pname = "scraper";
-      });
+        inherit (common) buildInputs;
+        nativeBuildInputs = l.attrValues {
+          inherit (pkgs) dioxus-cli tailwindcss;
+          inherit (config.treefmt.build.programs) rustfmt; # Yes, for some reason burn depends on rustfmt
+        };
+
+        strictDeps = true;
+
+        CARGO_BUILD_TARGET = app-config.build.target;
+        CARGO_BUILD_RUSTFLAGS = l.concatStringsSep " " app-config.build.rustflags;
+      };
+
+    packages.scraper = craneLib.buildPackage {
+      pname = "scraper";
+      version = readPackageVersion ./scraper/Cargo.toml;
+      src = l.fileset.toSource {
+        root = ./.;
+        fileset = l.fileset.unions [
+          ./Cargo.toml
+          ./Cargo.lock
+
+          ./scraper
+        ];
+      };
+
+      inherit (common) buildInputs;
+
+      strictDeps = true;
+      cargoExtraArgs = "-p scraper";
+    };
 
     devShells.rust = pkgs.mkShell {
       inputsFrom = [config.packages.app config.packages.scraper];
       packages = l.attrValues {
-        inherit (pkgs) dioxus-cli;
+        inherit (pkgs) tailwindcss-language-server;
         inherit (fenix) rust-analyzer;
         inherit (config.treefmt.build.programs) rustfmt;
       };
