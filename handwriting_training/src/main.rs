0cd991bc00ef0b33a4180ec2a7984e7ca5d62e2b
Justin Restivo <justin@restivo.me>
2024-06-17 16:32:38 -0400

    feat: activation functions and forward

--- handwriting_training/src/main.rs
+++ handwriting_training/src/main.rs
@@ -2,10 +2,30 @@
 
 static ARTIFACT_DIR: &str = "/tmp/burn-mylogs";
 static DATA_DIR: &str = "simple_data";
-use burn::{backend::ndarray::NdArrayDevice, module::Module, nn::{conv::{Conv2d, Conv2dConfig}, pool::{MaxPool2d, MaxPool2dConfig}, Dropout, DropoutConfig, Initializer, Linear, LinearConfig, PaddingConfig2d}, tensor::{activation::relu, backend::Backend, Float, Int, Tensor}};
+use burn::{backend::ndarray::NdArrayDevice, module::Module, nn::{conv::{Conv2d, Conv2dConfig}, pool::{MaxPool2d, MaxPool2dConfig}, Dropout, DropoutConfig, Initializer, Linear, LinearConfig, PaddingConfig2d, Relu}, tensor::{activation::relu, backend::Backend, Float, Int, Tensor}};
 
 const NUM_LABELS: usize = 10;
 const LEARNING_RATE: f64 = 1e-5;
+
+// temporary stopgap
+#[derive(Module, Clone, Debug, Default)]
+pub struct Tanh {}
+
+impl Tanh {
+    /// Create the module.
+    pub fn new() -> Self {
+        Self {}
+    }
+    /// Applies the forward pass on the input tensor.
+    ///
+    /// # Shapes
+    ///
+    /// - input: `[..., any]`
+    /// - output: `[..., any]`
+    pub fn forward<B: Backend, const D: usize>(&self, input: Tensor<B, D>) -> Tensor<B, D> {
+        burn::tensor::activation::tanh(input)
+    }
+}
 
 pub fn nll_todo<S>() -> S {
     None.unwrap()
@@ -30,11 +50,14 @@ pub fn nll_todo<S>() -> S {
 #[derive(Module, Debug)]
 pub struct HandwritingModel<B: Backend> {
     conv_2d_1: Conv2d<B>,
+    activation_1: Relu,
     max_pooling_2d_1: MaxPool2d,
     conv_2d_2: Conv2d<B>,
+    activation_2: Relu,
     max_pooling_2d_2: MaxPool2d,
     // flatten: Flatten<B>, -- implicit
     dense_1: Linear<B>,
+    activation_3: Tanh,
     dropout: Dropout,
     dense_2: Linear<B>
 }
@@ -54,6 +77,8 @@ impl<B: Backend> HandwritingModel<B> {
             },
             channels: nll_todo(),
         };
+
+        let activation_1 = Relu::new();
 
         let max_pooling_2d_1_config = MaxPool2dConfig {
             kernel_size: [2, 2], // assuming kernel_size == pool_size
@@ -82,6 +107,9 @@ impl<B: Backend> HandwritingModel<B> {
                 gain: 1.0,
             },
         };
+        let activation_2 = Relu::new();
+
+        let activation_3 = Tanh::new();
 
         let dense_1 = dense_1_config.init::<B>(device);
 
@@ -100,9 +128,12 @@ impl<B: Backend> HandwritingModel<B> {
 
         HandwritingModel {
             conv_2d_1,
+            activation_1,
             max_pooling_2d_1,
             conv_2d_2: nll_todo(),
             max_pooling_2d_2: nll_todo(),
+            activation_2,
+            activation_3,
             dense_1,
             dropout,
             dense_2,
@@ -110,7 +141,19 @@ impl<B: Backend> HandwritingModel<B> {
     }
 
 
-    pub fn forward(&self, input: Tensor<B, 1, Float>) {
+    // dims: [batch_size, channels, height, width]
+    pub fn forward(&self, input: Tensor<B, 4, Float>) -> Tensor<B, 2> {
+        let x = self.conv_2d_1.forward(input);
+        let x = self.activation_1.forward(x);
+        let x = self.max_pooling_2d_1.forward(x);
+        let x = self.conv_2d_2.forward(x);
+        let x = self.activation_2.forward(x);
+        let x = self.max_pooling_2d_2.forward(x);
+        let x = Tensor::flatten(x, 1, 3);
+        let x = self.dense_1.forward(x);
+        let x = self.activation_3.forward(x);
+        let x = self.dropout.forward(x);
+        self.dense_2.forward(x)
     }
 }
 
